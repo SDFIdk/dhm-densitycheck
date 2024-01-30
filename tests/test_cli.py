7a8e6061c116304a89ec3902cd7133bb8dc161fb
Peter Limkilde Svendsen <34237736+plimkilde@users.noreply.github.com>
2024-01-30 15:02:03 +0100

    Test end-to-end file read/write (#6)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,31 @@
+from osgeo import gdal, osr
+import numpy as np
+
 import subprocess
 
+from densitycheck.density import NODATA_VALUE
+
+gdal.UseExceptions()
+osr.UseExceptions()
+
+def call_point_density(args):
+    subprocess.check_call(['point_density'] + args)
+
 def test_cli_help():
-    subprocess.check_call(['point_density', '-h'])
+    call_point_density(['-h'])
+
+def test_cli_full(input_filename, output_filename, return_kind, expected_raster, osr_spatialreference):
+    call_point_density([input_filename, output_filename, '--cell-size', '500.0', '--returns', return_kind.name])
+
+    output_dataset = gdal.Open(output_filename)
+    output_srs = osr.SpatialReference(output_dataset.GetProjection())
+    output_geotransform = output_dataset.GetGeoTransform()
+    output_band = output_dataset.GetRasterBand(1)
+    output_nodata_value = output_band.GetNoDataValue()
+    output_array = output_band.ReadAsArray()
+
+    assert output_srs.IsSame(osr_spatialreference)
+    np.testing.assert_allclose(output_geotransform, [600000.0, 500.0, 0.0, 6201000.0, 0.0, -500.0])
+    assert (output_dataset.RasterYSize, output_dataset.RasterXSize) == expected_raster.shape
+    assert output_nodata_value == NODATA_VALUE
+    np.testing.assert_allclose(output_array, expected_raster)
